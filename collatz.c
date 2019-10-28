21e46df06650d72e22b2d600581e7f496419b53b
ömer faruk Arslan <39068840+MrARSLAN52@users.noreply.github.com>
2019-10-28 22:21:32 +0300

    Add files via upload

--- collatz.c
+++ collatz.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
+
+
 
 void program(int sayi);
 
+int sayiiste()
+{
+	
+	int gelen;
+	printf("pozitif bir sayı giriniz : ");
+	scanf("%d",&gelen);
+	return gelen;
+    
+}
+
 void  main(void)
 {
-     int girilen;
-     printf("sayi : ");
-     scanf("%d",&girilen);
-     program(girilen);
-
-     return 0;
+    int girilen = 0;
+	
+	do
+	{
+		girilen = sayiiste();
+	}
+	while(girilen <= 0);
+	
+	program(girilen);
+	
+    return 0;
 }
+
+
 
 void program(int sayi)
 {
-	printf("%d ",sayi);
-	if(sayi == 1) return;
-	pid_t  pid;
-	pid = fork();
-
+	
+	pid_t pid = fork();
+	
 	while(pid == -1)
 	{
 		pid = fork();
 	}
-
+	
 	if(pid == 0)
 	{
-		if(sayi % 2 == 0)
+		
+		printf("%d ",sayi);
+		if(sayi == 1) return;
+		
+		else if(sayi % 2 == 0)
 		{
 			sayi = sayi / 2;
 		}
@@ -36,11 +58,17 @@ void program(int sayi)
 		{
 			sayi = sayi * 3 + 1;
 		}
+			
+		program(sayi);
 	}
 	else
 	{
-		wait(NULL);
-		program(sayi);
+		
+		wait(&pid);
+		kill(getpid(),SIGKILL);
 	}
 
 }
+
+
+
